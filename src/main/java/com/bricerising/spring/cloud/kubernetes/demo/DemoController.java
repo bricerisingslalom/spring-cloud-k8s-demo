03be251b7eaa91bb19804e34c5032c4ef67d37b6
Brice Rising <brice.rising@slalom.com>
2019-07-26 16:15:22 -0400

    Use service discovery
    I added an endpoint that show's all the discovered services

--- src/main/java/com/bricerising/spring/cloud/kubernetes/demo/DemoController.java
+++ src/main/java/com/bricerising/spring/cloud/kubernetes/demo/DemoController.java
@@ -1,6 +1,10 @@
 package com.bricerising.spring.cloud.kubernetes.demo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +13,9 @@ public class DemoController {
 
     @Value("${demo.configmap.value}")
     private String configValue;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
 
         @RequestMapping("/status")
         public String status() {
@@ -17,6 +24,10 @@ public class DemoController {
         @RequestMapping("/config")
         public String config() {
             return configValue;
+        }
+        @RequestMapping("/services")
+        public List<String> services() {
+            return discoveryClient.getServices();
         }
 
 }
