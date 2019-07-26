dbc9b288f722a412d8dc9f7dd7bdc82cdb561177
Brice Rising <brice.rising@slalom.com>
2019-07-26 15:45:22 -0400

    Have app pull properties from configmap

--- src/main/java/com/bricerising/spring/cloud/kubernetes/demo/DemoController.java
+++ src/main/java/com/bricerising/spring/cloud/kubernetes/demo/DemoController.java
@@ -1,14 +1,22 @@
 package com.bricerising.spring.cloud.kubernetes.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-        @RequestMapping("/")
-        public String kuberenetes() {
-                return "Hello Kubernetes!";
+    @Value("${demo.configmap.value}")
+    private String configValue;
+
+        @RequestMapping("/status")
+        public String status() {
+                return "Alive!";
+        }
+        @RequestMapping("/config")
+        public String config() {
+            return configValue;
         }
 
 }
